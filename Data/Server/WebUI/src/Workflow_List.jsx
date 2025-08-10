33058e47868a598151e1ca36ea0e6d8baf8eb51d
Nicole Rappe <nicole.rappe@bunny-lab.io>
2025-08-10 00:39:08 -0600

    fix: correct tree view drag-and-drop import

--- Data/Server/WebUI/src/Workflow_List.jsx
+++ Data/Server/WebUI/src/Workflow_List.jsx
@@ -20,7 +20,7 @@ import {
   TreeItem,
   useTreeViewApiRef
 } from "@mui/x-tree-view";
-import { useTreeViewDragAndDrop } from "@mui/x-tree-view/TreeView";
+import { useTreeViewDragAndDrop } from "@mui/x-tree-view/useTreeViewDragAndDrop";
 import { RenameWorkflowDialog, RenameFolderDialog } from "./Dialogs";
 
 function buildTree(workflows) {
